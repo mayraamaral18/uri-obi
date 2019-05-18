a2d5123567f31414f52089bc412cc1b6c14b30d2
Mayra Amaral <38574181+mayraamaral18@users.noreply.github.com>
2019-05-18 04:51:09 -0900

    Add files via upload

---
+++ lista3-ex2.py
@@ -0,0 +1,6 @@
+c = 0
+for n in range(6):
+    valor = float(input())
+    if valor > 0:
+        c+=1
+print(c, "valores positivos")
---
+++ lista3-ex3.py
@@ -0,0 +1,12 @@
+senha = 2002
+
+resposta = int(input())
+
+while resposta != senha:
+    print("Senha Invalida")
+    resposta = int(input())
+print("Acesso Permitido")
+
+    
+
+    
